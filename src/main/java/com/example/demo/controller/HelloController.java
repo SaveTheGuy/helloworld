76f5e82779231e5019b8e9e97d7556196c74c8d7
44180 <44180@MSI>
2020-09-24 08:46:17 +0800

    responsebody

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -3,13 +3,14 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/hello")
 public class HelloController {
 	
 	@GetMapping("/helloworld/method")
-	public String helloworld() {
+	public @ResponseBody String helloworld() {
 		System.out.println("111");
 		return "hello world";
 	}
